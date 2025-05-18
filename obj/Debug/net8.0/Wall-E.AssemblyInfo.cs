6fb396d321e0a7494b58cd6328603e344a24b30d
AmandaMedina17 <rodriguezgilberto2513@gmail.com>
2025-05-18 01:04:57 -0400

    Initial commit

--- obj/Debug/net8.0/Wall-E.AssemblyInfo.cs
+++ obj/Debug/net8.0/Wall-E.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wall-E")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e92740bd48f62353884f94a5eb96b8d5f86e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wall-E")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wall-E")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
